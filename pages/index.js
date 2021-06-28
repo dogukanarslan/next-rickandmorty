3065ecc38fdad73d3ca22f1ed2f3ca8e3286ef3a
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-06-28 22:08:19 +0300

    Get characters

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Table.module.css';
 
-export default function Home() {
-  return <div>Homepage</div>;
+export async function getStaticProps() {
+  const res = await fetch('https://rickandmortyapi.com/api/character');
+  const data = await res.json();
+
+  return {
+    props: {
+      data
+    }
+  };
+}
+
+export default function Home(props) {
+  const {
+    data: { results: characters }
+  } = props;
+
+  const headers = ['Name', 'Status', 'Species', 'Type', 'Gender'];
+
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          {headers.map((header) => (
+            <th key={header}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {characters.map((character) => (
+          <tr key={character.id}>
+            <td>{character.name}</td>
+            <td>{character.status}</td>
+            <td>{character.species}</td>
+            <td>{character.type}</td>
+            <td>{character.gender}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
