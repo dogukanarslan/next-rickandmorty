72f52725952913f913d5be367013f7272bfa6a88
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-07-07 23:35:48 +0300

    Add pagination buttons to episodes

--- pages/episodes/index.js
+++ pages/episodes/index.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import styles from '../../styles/Table.module.css';
+import Button from '../../components/Button';
 
 export async function getStaticProps() {
   const res = await fetch(`${process.env.RICKANDMORTY_API}/episode`);
@@ -14,7 +15,7 @@ export async function getStaticProps() {
 
 export default function Episode(props) {
   const {
-    data: { results: episodes }
+    data: { results: episodes, info }
   } = props;
 
   const router = useRouter();
@@ -22,26 +23,35 @@ export default function Episode(props) {
   const headers = ['Name', 'Air Date', 'Episode'];
 
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          {headers.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {episodes.map((episode) => (
-          <tr
-            key={episode.id}
-            onClick={() => router.push(`${router.pathname}/${episode.id}`)}
-          >
-            <td>{episode.name}</td>
-            <td>{episode.air_date}</td>
-            <td>{episode.episode}</td>
+    <>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {headers.map((header) => (
+              <th key={header}>{header}</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {episodes.map((episode) => (
+            <tr
+              key={episode.id}
+              onClick={() => router.push(`${router.pathname}/${episode.id}`)}
+            >
+              <td>{episode.name}</td>
+              <td>{episode.air_date}</td>
+              <td>{episode.episode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="d-flex justify-content-between">
+        <Button label="Previous" />
+        <span>
+          {episodes.length} / {info.count}
+        </span>
+        <Button label="Next" />
+      </div>
+    </>
   );
 }
