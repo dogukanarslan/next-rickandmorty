98497f06d997b50311b74d35a9bb954a59e19c33
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-07-08 22:52:05 +0300

    Add pagination buttons to locations

--- pages/locations/index.js
+++ pages/locations/index.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import styles from '../../styles/Table.module.css';
+import Button from '../../components/Button';
 
 export async function getStaticProps() {
   const res = await fetch(`${process.env.RICKANDMORTY_API}/location`);
@@ -14,7 +15,7 @@ export async function getStaticProps() {
 
 export default function Location(props) {
   const {
-    data: { results: locations }
+    data: { results: locations, info }
   } = props;
 
   const router = useRouter();
@@ -22,26 +23,35 @@ export default function Location(props) {
   const headers = ['Name', 'Type', 'Dimension'];
 
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          {headers.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {locations.map((location) => (
-          <tr
-            key={location.id}
-            onClick={() => router.push(`${router.pathname}/${location.id}`)}
-          >
-            <td>{location.name}</td>
-            <td>{location.type}</td>
-            <td>{location.dimension}</td>
+    <>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {headers.map((header) => (
+              <th key={header}>{header}</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {locations.map((location) => (
+            <tr
+              key={location.id}
+              onClick={() => router.push(`${router.pathname}/${location.id}`)}
+            >
+              <td>{location.name}</td>
+              <td>{location.type}</td>
+              <td>{location.dimension}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="d-flex justify-content-between">
+        <Button label="Previous" />
+        <span>
+          {locations.length} / {info.count}
+        </span>
+        <Button label="Next" />
+      </div>
+    </>
   );
 }
