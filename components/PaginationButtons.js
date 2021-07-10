3c510aa3c7867f12a33e4a839b9a51e86b74525e
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-07-10 10:33:24 +0300

    Add style to pagination buttons

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -1,12 +1,13 @@
 import Button from './Button';
+import styles from '../styles/PaginationButtons.module.css';
 
 const PaginationButtons = (props) => {
   const { data, info } = props;
 
   return (
-    <div className="d-flex justify-content-between">
+    <div className={styles.container}>
       <Button label="Previous" />
-      <span>
+      <span className={styles.page}>
         {data.length} / {info.count}
       </span>
       <Button label="Next" />
