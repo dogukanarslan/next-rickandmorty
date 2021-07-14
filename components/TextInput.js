b5f4cb4e9ef6f8d6fb479847b014557c65af4ecb
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-07-14 18:26:20 +0300

    Add text input

---
+++ components/TextInput.js
@@ -0,0 +1,14 @@
+import styles from '../styles/TextInput.module.css';
+
+const TextInput = (props) => {
+  const { label, onChange, placeholder, options } = props;
+
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.label}>{label}</label>
+      <input className={styles.input} type="text" placeholder={placeholder} onChange={onChange} />
+    </div>
+  );
+};
+
+export default TextInput;
