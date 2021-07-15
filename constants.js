56f1ba5c9f91483e75973d87e632ace03f929d4a
Dogukan Arslan <dogukan2arslan@hotmail.com>
2021-07-15 11:10:51 +0300

    Add character filters

---
+++ constants.js
@@ -0,0 +1,13 @@
+export const filters = {
+  statuses: [
+    { name: 'Alive', value: 'alive' },
+    { name: 'Dead', value: 'dead' },
+    { name: 'Unknown', value: 'unknown' }
+  ],
+  genders: [
+    { name: 'Female', value: 'female' },
+    { name: 'Male', value: 'male' },
+    { name: 'Genderless', value: 'genderless' },
+    { name: 'Unkown', value: 'unknown' }
+  ]
+};
--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
 import styles from '../../styles/Table.module.css';
+import { filters } from '../../constants';
 import PaginationButtons from '../../components/PaginationButtons';
+import SelectInput from '../../components/SelectInput';
 
 export async function getStaticProps() {
   const res = await fetch(`${process.env.RICKANDMORTY_API}/character`);
@@ -24,34 +26,57 @@ export default function Home(props) {
 
   return (
     <>
-      <div className="scrollable-container">
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              {headers.map((header) => (
-                <th key={header}>{header}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {characters.map((character) => (
-              <tr
-                key={character.id}
-                onClick={() =>
-                  router.push(`${router.pathname}/${character.id}`)
-                }
-              >
-                <td>{character.name}</td>
-                <td>{character.status}</td>
-                <td>{character.species}</td>
-                <td>{character.type}</td>
-                <td>{character.gender}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="container">
+        <div className="row">
+          <div className="col col-6">
+            <SelectInput options={filters.statuses} label="Status" />
+          </div>
+          <div className="col col-6">
+            <SelectInput options={filters.genders} label="Gender" />
+          </div>
+        </div>
       </div>
-      <PaginationButtons data={characters} info={info} />
+
+      <div className="container">
+        <div className="row">
+          <div className="col col-12">
+            <div className="scrollable-container">
+              <table className={styles.table}>
+                <thead>
+                  <tr>
+                    {headers.map((header) => (
+                      <th key={header}>{header}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {characters.map((character) => (
+                    <tr
+                      key={character.id}
+                      onClick={() =>
+                        router.push(`${router.pathname}/${character.id}`)
+                      }
+                    >
+                      <td>{character.name}</td>
+                      <td>{character.status}</td>
+                      <td>{character.species}</td>
+                      <td>{character.type}</td>
+                      <td>{character.gender}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12">
+            <PaginationButtons data={characters} info={info} />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
