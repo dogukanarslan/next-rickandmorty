c8d8f66343dca84a09c11cabef92e00b140d4333
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-02-03 20:33:25 +0300

    Add prettier

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,31 +1,31 @@
-import styles from "../styles/Sidebar.module.css"
-import Link from "next/link"
+import styles from '../styles/Sidebar.module.css';
+import Link from 'next/link';
 
 const Sidebar = (props) => {
-  const { selected } = props
+  const { selected } = props;
 
   return (
     <div className={styles.sidebar}>
       <Link
         href="/characters"
-        className={selected === "/characters" ? styles.active : ""}
+        className={selected === '/characters' ? styles.active : ''}
       >
         Characters
       </Link>
       <Link
         href="/locations"
-        className={selected === "/locations" ? styles.active : ""}
+        className={selected === '/locations' ? styles.active : ''}
       >
         Locations
       </Link>
       <Link
         href="/episodes"
-        className={selected === "/episodes" ? styles.active : ""}
+        className={selected === '/episodes' ? styles.active : ''}
       >
         Episodes
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
