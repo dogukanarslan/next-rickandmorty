da13701cf936ea65310d5cb045fff5d39bfca4ca
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-05-10 19:04:32 +0300

    Migrate episode detail to app router

--- app/episodes/[id]/page.js
+++ app/episodes/[id]/page.js
@@ -11,9 +11,19 @@ const EpisodeDetailPage = async ({ params: { id } }) => {
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.details}>
-        <h1>{name}</h1>
+      <h1 className={styles.label}>Episode</h1>
+
+      <div className={styles.boxItem}>
+        <h1 className={styles.boxItemHeading}>Name</h1>
+        <p>{name}</p>
+      </div>
+
+      <div className={styles.boxItem}>
+        <h1 className={styles.boxItemHeading}>Air Date</h1>
         <p>{air_date}</p>
+      </div>
+      <div className={styles.boxItem}>
+        <div className={styles.boxItemHeading}>Episode</div>
         <p>{episode}</p>
       </div>
     </div>
