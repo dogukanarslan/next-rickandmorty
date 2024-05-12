8367cda69975af63b593d4c12797f2a3e76ebb66
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-05-13 00:36:56 +0300

    Add button color

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,20 @@
 import styles from '../styles/Button.module.css';
 
 const Button = (props) => {
-  const { label, ...args } = props;
+  const { label, color = 'secondary', ...args } = props;
+
+  let classes = `${styles.btn}`;
+
+  if (color === 'primary') {
+    classes += ` ${styles.primary}`;
+  }
+
+  if (color === 'secondary') {
+    classes += ` ${styles.secondary}`;
+  }
+
   return (
-    <button className={styles.btn} {...args}>
+    <button className={classes} {...args}>
       {label}
     </button>
   );
