f2ef4199dd3cb0613442eb84d9aa385fb259e692
Dogukan Arslan <dogukan2arslan@hotmail.com>
2024-05-12 12:55:53 +0300

    Convert locations table into card

---
+++ app/locations/page.js
@@ -0,0 +1,38 @@
+import { generateQuery } from '../../utils';
+
+import LocationsForm from '../../components/LocationsForm';
+import LocationList from '../../components/LocationList';
+import PaginationButtons from '../../components/PaginationButtons';
+
+const getData = async (params) => {
+  let url = `${process.env.RICKANDMORTY_API}/location?`;
+
+  const { page = 1, name = '', dimension = '' } = params;
+
+  url += generateQuery([
+    { label: 'page', value: page },
+    { label: 'name', value: name },
+    { label: 'dimension', value: dimension }
+  ]);
+
+  const res = await fetch(url);
+
+  return res.json();
+};
+
+const LocationsPage = async (props) => {
+  const { results: locations, info } = await getData(props.searchParams);
+
+  return (
+    <>
+      <LocationsForm />
+      <LocationList locations={locations} />
+      <PaginationButtons
+        info={info}
+        currentPage={props.searchParams.page || 1}
+      />
+    </>
+  );
+};
+
+export default LocationsPage;
